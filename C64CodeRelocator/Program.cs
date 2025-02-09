963021f1968207e131b94df65bcec1a4f9c351f2
Ian Wigley <ian.wigley@outlook.com>
2025-02-09 10:00:32 +0000

    Update Program.cs

--- C64CodeRelocator/Program.cs
+++ C64CodeRelocator/Program.cs
@@ -13,7 +13,7 @@ namespace C64CodeRelocator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new C64BinaryToAssemblyConverter());
         }
     }
 }
