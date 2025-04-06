672d377c299c2719b7ca3936807af589099168aa
Ian Wigley <ian.wigley@outlook.com>
2025-04-06 09:53:37 +0100

    Create TestAssemblyCreator.cs

---
+++ C64CodeRelocatorTests/TestAssemblyCreator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C64CodeRelocator;
+
+namespace C64CodeRelocatorTests
+{
+    [TestClass]
+    public class TestAssemblyCreator
+    {
+        [TestMethod]
+        public void TestSomething()
+        {
+            AssemblyCreator assemblyCreator = new AssemblyCreator();
+            assemblyCreator.InitialPass();
+        }
+    }
+}
