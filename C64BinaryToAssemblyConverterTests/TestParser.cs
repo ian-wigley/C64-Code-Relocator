d93efcf30286af70d9627b203d0c059d10c4b030
Ian Wigley <ian.wigley@outlook.com>
2025-04-07 18:44:39 +0100

    Add test config

---
+++ C64BinaryToAssemblyConverterTests/TestParser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace C64CodeRelocatorTests
+{
+    [TestClass]
+    public class TestParser
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
