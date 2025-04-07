59a69fb9907eeb5c01b2b13041353a885cf4e83f
Ian Wigley <ian.wigley@outlook.com>
2025-04-07 18:30:33 +0100

    Add renamed project files

---
+++ C64BinaryToAssemblyConverter/MemorySelector.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace C64CodeRelocator
+{
+    partial class MemorySelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox8 = new System.Windows.Forms.ComboBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            //
+            // groupBox1
+            //
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(32, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(208, 62);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Memory Start Address";
+            //
+            // comboBox4
+            //
+            this.comboBox4.DropDownWidth = 30;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(154, 30);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(40, 21);
+            this.comboBox4.TabIndex = 4;
+            //
+            // comboBox3
+            //
+            this.comboBox3.DropDownWidth = 30;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(108, 30);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(40, 21);
+            this.comboBox3.TabIndex = 3;
+            //
+            // comboBox2
+            //
+            this.comboBox2.DropDownWidth = 30;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(62, 30);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(40, 21);
+            this.comboBox2.TabIndex = 2;
+            //
+            // comboBox1
+            //
+            this.comboBox1.DropDownWidth = 30;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(40, 21);
+            this.comboBox1.TabIndex = 1;
+            //
+            // groupBox2
+            //
+            this.groupBox2.Controls.Add(this.comboBox8);
+            this.groupBox2.Controls.Add(this.comboBox7);
+            this.groupBox2.Controls.Add(this.comboBox6);
+            this.groupBox2.Controls.Add(this.comboBox5);
+            this.groupBox2.Location = new System.Drawing.Point(32, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(208, 72);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select Memory End Address";
+            //
+            // comboBox8
+            //
+            this.comboBox8.FormattingEnabled = true;
+            this.comboBox8.Location = new System.Drawing.Point(154, 29);
+            this.comboBox8.Name = "comboBox8";
+            this.comboBox8.Size = new System.Drawing.Size(40, 21);
+            this.comboBox8.TabIndex = 8;
+            //
+            // comboBox7
+            //
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Location = new System.Drawing.Point(108, 29);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(40, 21);
+            this.comboBox7.TabIndex = 7;
+            //
+            // comboBox6
+            //
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(62, 29);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(40, 21);
+            this.comboBox6.TabIndex = 6;
+            //
+            // comboBox5
+            //
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(16, 29);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(40, 21);
+            this.comboBox5.TabIndex = 5;
+            //
+            // button1
+            //
+            this.button1.Location = new System.Drawing.Point(94, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            //
+            // checkBox1
+            //
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(48, 170);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(173, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Convert Illegal opcodes to data";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            //
+            // MemorySelector
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 245);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MemorySelector";
+            this.Text = "MemorySelector";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox8;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
---
+++ C64BinaryToAssemblyConverter/MemorySelector.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace C64CodeRelocator
+{
+    public partial class MemorySelector : Form
+    {
+        private readonly IList<object> memValues = new List<object>();
+        public string GetSelectedMemStartLocation { get { return comboBox1.Text + comboBox2.Text + comboBox3.Text + comboBox4.Text; } }
+        public string GetSelectedMemEndLocation { get { return comboBox5.Text + comboBox6.Text + comboBox7.Text + comboBox8.Text; } }
+
+        public MemorySelector(char[] startAdress, char[] endAdress)
+        {
+            InitializeComponent();
+
+            MaximizeBox = false;
+            MinimizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            for (int i = 0; i < 16; i++)
+            {
+                memValues.Add(new { Text = i.ToString("X1"), Value = i });
+            }
+
+            InitialiseComboBoxes(comboBox1);
+            InitialiseComboBoxes(comboBox2);
+            InitialiseComboBoxes(comboBox3);
+            InitialiseComboBoxes(comboBox4);
+
+            InitialiseComboBoxes(comboBox5);
+            InitialiseComboBoxes(comboBox6);
+            InitialiseComboBoxes(comboBox7);
+            InitialiseComboBoxes(comboBox8);
+
+            comboBox1.Text = startAdress[0].ToString();
+            comboBox2.Text = startAdress[1].ToString();
+            comboBox3.Text = startAdress[2].ToString();
+            comboBox4.Text = startAdress[3].ToString();
+
+            comboBox5.Text = endAdress[0].ToString();
+            comboBox6.Text = endAdress[1].ToString();
+            comboBox7.Text = endAdress[2].ToString();
+            comboBox8.Text = endAdress[3].ToString();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        private void InitialiseComboBoxes(ComboBox comboBox)
+        {
+            comboBox.BindingContext = new BindingContext();
+            comboBox.DisplayMember = "Text";
+            comboBox.ValueMember = "Value";
+            comboBox.DataSource = memValues;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        private void Button1_Click(object sender, System.EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
