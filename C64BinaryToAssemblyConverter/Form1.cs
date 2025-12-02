860eef5854307814d1fe75ff4e8bb34e60c0bb3d
Ian Wigley <ian.wigley@outlook.com>
2025-12-02 17:46:06 +0000

    Update Form1.cs

--- C64BinaryToAssemblyConverter/Form1.cs
+++ C64BinaryToAssemblyConverter/Form1.cs
@@ -51,31 +51,31 @@ namespace C64BinaryToAssemblyConverter
         /// </summary>
         private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog
+            var openFileDialog = new OpenFileDialog
             {
-                Title = "Open File",
+                Title = @"Open File",
                 InitialDirectory = @"*.*",
-                Filter = "All files (*.prg)|*.PRG|All files (*.*)|*.*",
+                Filter = @"All files (*.prg)|*.PRG|All files (*.*)|*.*",
                 FilterIndex = 2,
                 RestoreDirectory = true
             };
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                ClearCollections();
-                textBox1.Clear();
-                MemoryLocation ml = new MemoryLocation();
-                if (ml.ShowDialog() == DialogResult.OK)
-                {
-                    // Use a monospaced font
-                    textBox1.Font = new Font(FontFamily.GenericMonospace, textBox1.Font.Size);
-                    _ = int.TryParse(ml.GetMemStartLocation, out int startAddress);
-                    data = parser.LoadBinaryData(openFileDialog.FileName);
-                    textBox1.Lines = parser.ParseFileContent(data, textBox1, startAddress, ref lineNumbers); //, ref code);
-                    GenerateLabels.Enabled = true;
-                    leftWindowToolStripMenuItem.Enabled = true;
-                }
-            }
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            ClearCollections();
+            textBox1.Clear();
+            var ml = new LoadIntoMemoryLocationSelector();
+            if (ml.ShowDialog() != DialogResult.OK) return;
+            // Use a monospaced font
+            textBox1.Font = new Font(FontFamily.GenericMonospace, textBox1.Font.Size);
+            _ = int.TryParse(ml.GetMemStartLocation, out var startAddress);
+            data = parser.LoadBinaryData(openFileDialog.FileName);
+            textBox1.Lines = parser.ParseFileContent(data, textBox1, startAddress, ref lineNumbers);
+            
+            dataStatements = parser.DataStatements;
+            illegalOpcodes = parser.IllegalOpCodes;            
+            
+            GenerateLabels.Enabled = true;
+            leftWindowToolStripMenuItem.Enabled = true;
         }
 
         /// <summary>
@@ -91,89 +91,90 @@ namespace C64BinaryToAssemblyConverter
         /// </summary>
         private void GenerateLabelsClickEvent(object sender, EventArgs e)
         {
-            char[] startAdress = new char[lineNumbers[0].Length];
-            char[] endAdress = new char[lineNumbers[lineNumbers.Count - 1].Length];
-            int firstOccurance = 0;
-            int lastOccurrance = 0;
+            char[] startAddress = new char[lineNumbers[0].Length];
+            char[] endAddress = new char[lineNumbers[lineNumbers.Count - 1].Length];
+            int firstOccurence = 0;
+            int lastOccurrence = 0;
 
             int count = 0;
             foreach (char chr in lineNumbers[0])
             {
-                startAdress[count++] = chr;
+                startAddress[count++] = chr;
             }
             count = 0;
             foreach (char chr in lineNumbers[lineNumbers.Count - 1])
             {
-                endAdress[count++] = chr;
+                endAddress[count++] = chr;
             }
 
-            MemorySelector ms = new MemorySelector(startAdress, endAdress);
-            if (ms.ShowDialog() == DialogResult.OK)
+            var ms = new MemoryLocationsToConvertSelector(startAddress, endAddress);
+            if (ms.ShowDialog() != DialogResult.OK) return;
+            var start = int.Parse(ms.GetSelectedMemStartLocation, System.Globalization.NumberStyles.HexNumber);
+            var end = int.Parse(ms.GetSelectedMemEndLocation, System.Globalization.NumberStyles.HexNumber);
+
+            var dataStatmentsRequired = ms.GetConvertIllegalOpCodes;
+                
+            var delta = end - start;
+            var firstIllegalOpcodeFound = false;
+            var replacedWithDataStatements = new Dictionary<string, string[]>();
+
+            if (start <= end)
             {
-                int start = int.Parse(ms.GetSelectedMemStartLocation, System.Globalization.NumberStyles.HexNumber);
-                int end = int.Parse(ms.GetSelectedMemEndLocation, System.Globalization.NumberStyles.HexNumber);
-                int delta = end - start;
-                bool firstIllegalOpcodeFound = false;
-                Dictionary<string, string[]> replacedWithDataStatements = new Dictionary<string, string[]>();
-
-                if (start <= end)
+                //Check to see if illegal opcodes exist within the code selection
+                for (var i = start; i < end; i++)
                 {
-                    //Check to see if illegal opcodes exist within the code selection
-                    for (int i = start; i < end; i++)
+                    if (illegalOpcodes.Contains(i.ToString("X4")))
                     {
-                        if (illegalOpcodes.Contains(i.ToString("X4")))
+                        if (i > firstOccurence && !firstIllegalOpcodeFound)
                         {
-                            if (i > firstOccurance && !firstIllegalOpcodeFound)
-                            {
-                                firstOccurance = i;
-                                firstIllegalOpcodeFound = true;
-                            }
-                            if (i > lastOccurrance)
-                            {
-                                lastOccurrance = i;
-                            }
+                            firstOccurence = i;
+                            firstIllegalOpcodeFound = true;
+                        }
+                        if (i > lastOccurrence)
+                        {
+                            lastOccurrence = i;
                         }
                     }
-
-                    var temp = lastOccurrance.ToString("X4");
-                    int index = 0;
-                    foreach (string str in parser.Code)
-                    {
-                        if (str.Contains(temp))
-                        {
-                            // nudge the last Occurance along to the next valid opCode
-                            lastOccurrance = int.Parse(lineNumbers[++index], System.Globalization.NumberStyles.HexNumber);
-                        }
-                        index++;
-                    }
-
-                    for (int i = firstOccurance; i < lastOccurrance; i++)
-                    {
-                        // Replace the Illegal Opcodes with data statement
-                        if (dataStatements.TryGetValue(i.ToString("X4"), out string[] dataValue))
-                        {
-                            replacedWithDataStatements.Add(i.ToString("X4"), dataValue);
-                        }
-                    }
-
-                    DialogResult result = DialogResult.No;
-                    if (firstIllegalOpcodeFound)
-                    {
-                        result = MessageBox.Show("Illegal Opcodes found within the selection from : " + firstOccurance.ToString("X4") + " to " + lastOccurrance.ToString("X4") + "\n"
-                        + "Replace Illegal Opcodes with data statements ?", " ", MessageBoxButtons.YesNo);
-                    }
-
-                    bool convertToBytes = false;
-                    if (result == DialogResult.Yes)
-                    {
-                        convertToBytes = true;
-                    }
-                    AddLabels(delta, ms.GetSelectedMemStartLocation, ms.GetSelectedMemEndLocation, convertToBytes, replacedWithDataStatements);
                 }
-                else
+
+                var temp = lastOccurrence.ToString("X4");
+                int index = 0;
+                foreach (string str in parser.Code)
                 {
-                    MessageBox.Show("The selected end address exceeds the length of the bytes $" + lineNumbers[lineNumbers.Count - 1]);
+                    if (str.Contains(temp))
+                    {
+                        // nudge the last Occurrence along to the next valid opCode
+                        lastOccurrence = int.Parse(lineNumbers[++index], System.Globalization.NumberStyles.HexNumber);
+                    }
+                    index++;
                 }
+
+                for (int i = firstOccurence; i < lastOccurrence; i++)
+                {
+                    // Replace the Illegal Opcodes with data statement
+                    if (dataStatements.TryGetValue(i.ToString("X4"), out string[] dataValue))
+                    {
+                        replacedWithDataStatements.Add(i.ToString("X4"), dataValue);
+                    }
+                }
+
+                var result = DialogResult.No;
+                if (firstIllegalOpcodeFound)
+                {
+                    result = MessageBox.Show(@"Illegal Opcodes found within the selection from : " + firstOccurence.ToString("X4") + @" to " + lastOccurrence.ToString("X4") + @"\n"
+                                             + @"Replace Illegal Opcodes with data statements ?", @" ", MessageBoxButtons.YesNo);
+                }
+
+                var convertToBytes = false;
+                if (result == DialogResult.Yes)
+                {
+                    convertToBytes = true;
+                }
+                AddLabels(delta, ms.GetSelectedMemStartLocation, ms.GetSelectedMemEndLocation, convertToBytes, replacedWithDataStatements);
+            }
+            else
+            {
+                MessageBox.Show(@"The selected end address exceeds the length of the bytes $" + lineNumbers[lineNumbers.Count - 1]);
             }
         }
 
@@ -200,12 +201,12 @@ namespace C64BinaryToAssemblyConverter
         /// </summary>
         private void ClearRightWindow()
         {
-            assemblyCreator.passOne.Clear();
-            assemblyCreator.passTwo.Clear();
-            assemblyCreator.passThree.Clear();
-            assemblyCreator.found.Clear();
-            assemblyCreator.labelLoc.Clear();
-            assemblyCreator.branchLoc.Clear();
+            assemblyCreator.PassOne.Clear();
+            assemblyCreator.PassTwo.Clear();
+            assemblyCreator.PassThree.Clear();
+            assemblyCreator.Found.Clear();
+            assemblyCreator.LabelLocations.Clear();
+            assemblyCreator.BranchLocations.Clear();
         }
 
         /// <summary>
@@ -221,7 +222,7 @@ namespace C64BinaryToAssemblyConverter
         /// </summary>
         private void RightWindowToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Save(assemblyCreator.passThree);
+            Save(assemblyCreator.PassThree);
         }
 
         /// <summary>
@@ -231,9 +232,9 @@ namespace C64BinaryToAssemblyConverter
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog
             {
-                Title = "Save File",
+                Title = @"Save File",
                 InitialDirectory = @"*.*",
-                Filter = "All files (*.*)|*.*|All files (*.a)|*.a",
+                Filter = @"All files (*.*)|*.*|All files (*.a)|*.a",
                 FilterIndex = 2,
                 RestoreDirectory = true
             };
