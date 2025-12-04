96efbb6a830914d857b498f0cc5ea3f7ec031777
Ian Wigley <ian.wigley@outlook.com>
2025-12-04 14:53:46 +0000

    Update TestParser.cs

--- C64BinaryToAssemblyConverterTests/TestParser.cs
+++ C64BinaryToAssemblyConverterTests/TestParser.cs
@@ -1,16 +1,41 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
 using C64BinaryToAssemblyConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
 
-namespace C64CodeRelocatorTests
+namespace C64BinaryToAssemblyConverterTests
 {
     [TestClass]
     public class TestParser
     {
+        private Parser InstantiateParser()
+        {
+            return new Parser();
+        }
+        
         [TestMethod]
         public void TestParserObject()
         {
-            Parser parser = new Parser();
+            var parser = InstantiateParser();
             Assert.IsNotNull(parser);
+        }
+        
+        [TestMethod]
+        public void TestLoadBinaryData(){
+            var parser = InstantiateParser();
+            var result = parser.LoadBinaryData("");
+            Assert.IsTrue(result.Length.Equals(0));
+        }
+        
+        [TestMethod]
+        public void TestParseFileContent()
+        {
+            var parser = InstantiateParser();
+            var bytes = new byte[1024];
+            var list = new List<string>();
+            var textBox = new TextBox();
+            var result = parser.ParseFileContent(bytes, textBox, 0, ref list);
+            Assert.IsTrue(result.Length.Equals(0));
         }
     }
 }
