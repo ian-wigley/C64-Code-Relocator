69b4341b4a26649f5d9bfecf4e362e8922b917db
Ian Wigley <ian.wigley@outlook.com>
2026-02-12 16:57:14 +0000

    Update TestAssemblyCreator.cs

--- C64BinaryToAssemblyConverterTests/TestAssemblyCreator.cs
+++ C64BinaryToAssemblyConverterTests/TestAssemblyCreator.cs
@@ -1,5 +1,6 @@
 ﻿using C64BinaryToAssemblyConverter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace C64BinaryToAssemblyConverterTests
 {
@@ -9,13 +10,32 @@ namespace C64BinaryToAssemblyConverterTests
         [TestMethod]
         public void TestAssemblyCreatorObject()
         {
-            AssemblyCreator assemblyCreator = new AssemblyCreator();
+            var assemblyCreator = new AssemblyCreator();
             Assert.IsNotNull(assemblyCreator);
         }
         
-        public void TestLoadBinaryData()
+        [TestMethod]
+        public void TestAssemblyCreatorInitialPass()
         {
-            AssemblyCreator assemblyCreator = new AssemblyCreator();
+            var assemblyCreator = new AssemblyCreator();
+            assemblyCreator.InitialPass(0, 1, false, new Dictionary<string, string[]>());
+            Assert.IsNull(assemblyCreator.Code);
+            Assert.IsTrue(assemblyCreator.PassOne.Count == 0);
+        }
+
+        [TestMethod]
+        public void TestAssemblyCreatorSecondPass()
+        {
+            var assemblyCreator = new AssemblyCreator();
+            assemblyCreator.SecondPass();
+        }
+        
+        [TestMethod]
+        public void TestAssemblyCreatorFinalPass()
+        {
+            var assemblyCreator = new AssemblyCreator();
+            var result = assemblyCreator.FinalPass(new List<string>(), 0, "0800");
+            Assert.IsNotNull(result);
         }
     }
 }
