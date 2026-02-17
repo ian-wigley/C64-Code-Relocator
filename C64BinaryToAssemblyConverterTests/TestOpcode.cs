9ccee80da41ce15f9e868ce5b7e0682f49a6ff12
Ian Wigley <ian.wigley@outlook.com>
2026-02-17 15:17:17 +0000

    Create TestOpcode.cs

---
+++ C64BinaryToAssemblyConverterTests/TestOpcode.cs
@@ -0,0 +1,16 @@
+﻿using C64BinaryToAssemblyConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace C64BinaryToAssemblyConverterTests
+{
+    [TestClass]
+    public class TestOpcode
+    {
+        [TestMethod]
+        public void TestOpCodeObject()
+        {
+            var opCode = new OpCode("code", "name", 1, "prefix", "suffix", false);
+            Assert.IsNotNull(opCode);
+        }
+    }
+}
