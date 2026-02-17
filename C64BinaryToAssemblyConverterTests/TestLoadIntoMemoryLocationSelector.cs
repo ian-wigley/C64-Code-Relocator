146ddc4ca0470bd1e9abcab70e35da82c4ba31cb
Ian Wigley <ian.wigley@outlook.com>
2026-02-17 15:17:25 +0000

    Create TestLoadIntoMemoryLocationSelector.cs

---
+++ C64BinaryToAssemblyConverterTests/TestLoadIntoMemoryLocationSelector.cs
@@ -0,0 +1,15 @@
+﻿using C64BinaryToAssemblyConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace C64BinaryToAssemblyConverterTests
+{
+    [TestClass]
+    public class TestLoadIntoMemoryLocationSelector
+    {
+        [TestMethod]
+        public void TestLoadIntoMemoryLocationSelectorObject()
+        {
+
+        }
+    }
+}
