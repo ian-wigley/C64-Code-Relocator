01bf436a1baea53ceb63742a90c50a9c7122a10e
Ian Wigley <ian.wigley@outlook.com>
2026-03-24 18:04:46 +0000

    Create SettingsCache.cs

---
+++ C64BinaryToAssemblyConverter/SettingsCache.cs
@@ -0,0 +1,13 @@
+﻿namespace C64BinaryToAssemblyConverter
+{
+    public class SettingsCache
+    {
+        public SettingsCache(
+        int numberOfBytesPerLine
+        )
+        {
+            NumberOfBytesPerLine = numberOfBytesPerLine;
+        }
+        public int NumberOfBytesPerLine { get; private set; }
+    }
+}
