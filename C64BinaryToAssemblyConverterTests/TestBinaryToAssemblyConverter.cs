26f1d770ed7d07e6485ccb44468a11ad0650b8c3
Ian Wigley <ian.wigley@outlook.com>
2026-03-30 18:51:09 +0100

    Create TestBinaryToAssemblyConverter.cs

---
+++ C64BinaryToAssemblyConverterTests/TestBinaryToAssemblyConverter.cs
@@ -0,0 +1,26 @@
+﻿using C64BinaryToAssemblyConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace C64BinaryToAssemblyConverterTests
+{
+    [TestClass]
+    public class TestBinaryToAssemblyConverter
+    {
+        [TestMethod]
+        public void InstantiateC64BinaryToAssemblyConverter()
+        {
+            var binaryToAssemblyConverter = new C64BinaryToAssemblyConverter.C64BinaryToAssemblyConverter();
+            Assert.IsNotNull(binaryToAssemblyConverter);
+        }
+
+        [TestMethod]
+        public void TestConvertToDataBytesClick()
+        {
+            var binaryToAssemblyConverter = new C64BinaryToAssemblyConverter.C64BinaryToAssemblyConverter();
+            binaryToAssemblyConverter.ConvertToDataBytesClick(null, null);
+            Assert.IsNotNull(binaryToAssemblyConverter);
+        }
+
+    }
+}
