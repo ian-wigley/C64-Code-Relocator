f956629b1444e336b02ff991efb963a4dfcaabcb
Ian Wigley <ian.wigley@outlook.com>
2026-04-08 09:30:24 +0100

    Update TestBinaryToAssemblyConverter.cs

--- C64BinaryToAssemblyConverterTests/TestBinaryToAssemblyConverter.cs
+++ C64BinaryToAssemblyConverterTests/TestBinaryToAssemblyConverter.cs
@@ -1,6 +1,7 @@
 ﻿using C64BinaryToAssemblyConverter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace C64BinaryToAssemblyConverterTests
@@ -43,21 +44,32 @@ namespace C64BinaryToAssemblyConverterTests
         public void TestConvertToDataBytesClick()
         {
             PopulateOpCodeList.Init();
-            byte[] bytes = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            byte[] bytes = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
             var list = new List<string>();
             var textBox = new TextBox();
 
             var binaryToAssemblyConverter = new TestAssemblyConverter();
             binaryToAssemblyConverter._parser.ParseFileContent(bytes, textBox, 0, ref list);
 
-            var text = "0814  00          BRK\r\n0815  00          BRK\r\n0816  00          BRK\r\n0817  00          BRK\r\n0818  00          BRK\r\n0819  00          BRK\r\n081A  00          BRK\r\n081B  00          BRK\r\n081C  00          BRK\r\n081D  00          BRK\r\n081E  00          BRK\r\n081F  00          BRK";
+            //var text = "0814  00          BRK\r\n0815  00          BRK\r\n0816  00          BRK\r\n0817  00          BRK\r\n0818  00          BRK\r\n0819  00          BRK\r\n081A  00          BRK\r\n081B  00          BRK\r\n081C  00          BRK\r\n081D  00          BRK\r\n081E  00          BRK\r\n081F  00          BRK";
+
+            var start = 0x0814;
+            var text = "";
+            foreach (byte b in bytes) {
+                text += start.ToString("X4") + "  " + b.ToString("X2") + "          BRK\r\n";
+                start += 1;
+            }
+
 
             binaryToAssemblyConverter.SetSelected(text);
             binaryToAssemblyConverter.ConvertToDataBytesClick(null, null);
-
             var result = binaryToAssemblyConverter.GetSelected();
 
-            Assert.IsNotNull(binaryToAssemblyConverter);
+            string target = "$00";
+            int count = Enumerable.Range(0, result.Length - target.Length + 1)
+                .Count(i => result.Substring(i, target.Length) == target);
+
+            Assert.IsNotNull(count == bytes.Length);
         }
 
     }
