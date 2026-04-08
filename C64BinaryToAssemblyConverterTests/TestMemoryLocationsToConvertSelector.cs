e11aeaa9332754c40b1db6fc29df906705bf2cda
Ian Wigley <ian.wigley@outlook.com>
2026-04-08 18:57:09 +0100

    Update TestMemoryLocationsToConvertSelector.cs

--- C64BinaryToAssemblyConverterTests/TestMemoryLocationsToConvertSelector.cs
+++ C64BinaryToAssemblyConverterTests/TestMemoryLocationsToConvertSelector.cs
@@ -1,5 +1,4 @@
-﻿using C64BinaryToAssemblyConverter;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace C64BinaryToAssemblyConverterTests
 {
