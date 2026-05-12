6c6e41f9cefe48b84902a7e658dc2923f847c357
Ian Wigley <ian.wigley@outlook.com>
2026-05-12 18:47:51 +0100

    Create ConfigureSettings.cs

---
+++ C64BinaryToAssemblyConverter/ConfigureSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace C64BinaryToAssemblyConverter
+{
+    public partial class ConfigureSettings : Form
+    {
+        public ConfigureSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
