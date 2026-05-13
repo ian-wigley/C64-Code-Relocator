d5d325731e9e68448f15405245e998ab6729ab18
Ian Wigley <ian.wigley@outlook.com>
2026-05-13 18:14:52 +0100

    Update ConfigureSettings.Designer.cs

--- C64BinaryToAssemblyConverter/ConfigureSettings.Designer.cs
+++ C64BinaryToAssemblyConverter/ConfigureSettings.Designer.cs
@@ -29,19 +29,45 @@ namespace C64BinaryToAssemblyConverter
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Branch Label";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jump Label";
             // 
             // ConfigureSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 185);
+            this.ClientSize = new System.Drawing.Size(424, 141);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "ConfigureSettings";
             this.Text = "ConfigureSettings";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
