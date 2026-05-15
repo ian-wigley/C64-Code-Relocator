d3fd2fbcf6eafd996ac9839a78f95714b990c703
Ian Wigley <ian.wigley@outlook.com>
2026-05-15 15:01:22 +0100

    Update ConfigureSettings.cs

--- C64BinaryToAssemblyConverter/ConfigureSettings.cs
+++ C64BinaryToAssemblyConverter/ConfigureSettings.cs
@@ -4,9 +4,18 @@ namespace C64BinaryToAssemblyConverter
 {
     public partial class ConfigureSettings : Form
     {
-        public ConfigureSettings()
+        public ConfigureSettings(SettingsCache settingsCache)
         {
             InitializeComponent();
+            textBox1.Text = settingsCache.Branch;
+            textBox2.Text = settingsCache.Label;
+            textBox3.Text = settingsCache.NumberOfBytesPerLine.ToString();
+        }
+
+        private void ButtonClick(object sender, System.EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
         }
     }
 }
