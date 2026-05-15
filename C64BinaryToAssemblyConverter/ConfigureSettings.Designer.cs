bc361218b1f010bfbc95c2b488d516188e319323
Ian Wigley <ian.wigley@outlook.com>
2026-05-15 15:01:18 +0100

    Update ConfigureSettings.Designer.cs

--- C64BinaryToAssemblyConverter/ConfigureSettings.Designer.cs
+++ C64BinaryToAssemblyConverter/ConfigureSettings.Designer.cs
@@ -29,35 +29,85 @@ namespace C64BinaryToAssemblyConverter
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.BranchLabel = new System.Windows.Forms.Label();
+            this.JumpLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.NumberOfBytesPerLine = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // BranchLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Branch Label";
+            this.BranchLabel.AutoSize = true;
+            this.BranchLabel.Location = new System.Drawing.Point(26, 18);
+            this.BranchLabel.Name = "BranchLabel";
+            this.BranchLabel.Size = new System.Drawing.Size(70, 13);
+            this.BranchLabel.TabIndex = 0;
+            this.BranchLabel.Text = "Branch Label";
             // 
-            // label2
+            // JumpLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Jump Label";
+            this.JumpLabel.AutoSize = true;
+            this.JumpLabel.Location = new System.Drawing.Point(26, 44);
+            this.JumpLabel.Name = "JumpLabel";
+            this.JumpLabel.Size = new System.Drawing.Size(61, 13);
+            this.JumpLabel.TabIndex = 1;
+            this.JumpLabel.Text = "Jump Label";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // NumberOfBytesPerLine
+            // 
+            this.NumberOfBytesPerLine.AutoSize = true;
+            this.NumberOfBytesPerLine.Location = new System.Drawing.Point(26, 73);
+            this.NumberOfBytesPerLine.Name = "NumberOfBytesPerLine";
+            this.NumberOfBytesPerLine.Size = new System.Drawing.Size(127, 13);
+            this.NumberOfBytesPerLine.TabIndex = 5;
+            this.NumberOfBytesPerLine.Text = "Number of Bytes Per Line";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Okay";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonClick);
             // 
             // ConfigureSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 141);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(297, 141);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.NumberOfBytesPerLine);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.JumpLabel);
+            this.Controls.Add(this.BranchLabel);
             this.Name = "ConfigureSettings";
             this.Text = "ConfigureSettings";
             this.ResumeLayout(false);
@@ -67,7 +117,12 @@ namespace C64BinaryToAssemblyConverter
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label BranchLabel;
+        private System.Windows.Forms.Label JumpLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label NumberOfBytesPerLine;
+        private System.Windows.Forms.Button button1;
     }
 }
